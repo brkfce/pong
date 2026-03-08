564fe9a5250b98c5b09a018b43af71c8c41f3e36
brkfce <calsutton12@gmail.com>
2026-03-08 16:52:56 +0000

    Following basic example to add movement to player sprite.

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use bevy::{math::VectorSpace, prelude::*};
+
+#[derive(Component)]
+struct Player;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, move_player)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Text2d::new("@"),
+        TextFont {
+            font_size: 12.8,
+            font: default(),
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        Transform::from_translation(Vec3::ZERO),
+        Player,
+    ));
+}
+
+fn move_player(
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut player_transform: Single<&mut Transform, With<Player>>,
+) {
+    let mut direction = Vec2::ZERO;
+
+    if input.pressed(KeyCode::ArrowUp) {
+        direction.y += 1.0;
+    }
+    if input.pressed(KeyCode::ArrowDown) {
+        direction.y -= 1.0;
+    }
+
+    if direction != Vec2::ZERO {
+        let speed = 300.0; // pixels per second
+        let delta = direction.normalize() * speed * time.delta_secs();
+        player_transform.translation.y += delta.y;
+    }
+}
